1ba59e72c90d70bb13e7557c01dfdf925413c124
Tim Gangl <tim.gangl30@gmail.com>
2020-05-13 09:53:49 -0700

    added form for memorial entry

--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -5,19 +5,20 @@ import { Route } from 'react-router-dom';
 import Login from '../Login';
 import Register from '../Register';
 import About from '../pages/About';
-
+import Entry from '../pages/Entry'
 // STYLESHEETS
 import "./wrapper.css";
 
 const Wrapper = () => {
-    return (
-        <div className="wrapper">
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
+  return (
+    <div className="wrapper">
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
 
-            <Route path="/about" component={About} />
-        </div>
-    );
+      <Route path="/about" component={About} />
+      <Route path="/entry" component={Entry} />
+    </div>
+  );
 };
 
 export default Wrapper;
