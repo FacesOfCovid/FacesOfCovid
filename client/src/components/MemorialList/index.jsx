a9764087cd1cd452f5b2cbbd09049429405c7483
Jed Smit <jedsmit1@gmail.com>
2020-06-04 13:22:14 -0700

    add useInfiniteScroll custom hook

--- client/src/components/MemorialList/index.jsx
+++ client/src/components/MemorialList/index.jsx
@@ -1,15 +1,90 @@
-import React from 'react';
+// import React from 'react';
 
-const MemorialList = (props) => {
+// const MemorialList = (props) => {
 
+//     return (
+//         <div className="MemorialList">
+//             <ul className="list-group mb-2">
+//                 {props.entries.map(entry => <li className="list-group-item"><img src={entry}></img></li>)}
+//             </ul>
+//             {props.msg}
+//         </div>
+//     )
+// }
+
+// export default MemorialList;
+import React, { useState } from "react";
+import { useInfiniteScroll } from "react-infinite-scroll-hook";
+
+
+const ARRAY_SIZE = 20;
+const RESPONSE_TIME = 1000;
+
+function PrintImg() {
     return (
-        <div className="MemorialList">
-            <ul className="list-group mb-2">
-                {props.entries.map(entry => <li className="list-group-item"><img src={entry}></img></li>)}
-            </ul>
-            {props.msg}
-        </div>
-    )
+        <img
+            src={
+                "https://exitocol.vteximg.com.br/assets/vtex.file-manager-graphql/images/slider-blue-week-desktop___451bdd92df31482e0ece795773e265d9.jpg"
+            }
+        />
+    );
 }
 
-export default MemorialList;
+function loadItems(prevArray = [], startCursor = 0) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let newArray = prevArray;
+
+            for (let i = startCursor; i < startCursor + ARRAY_SIZE; i++) {
+                const newItem = {
+                    key: i,
+                    value: `This is item ${i}`
+                };
+                newArray = [...newArray, newItem];
+            }
+
+            resolve(newArray);
+        }, RESPONSE_TIME);
+    });
+}
+
+function InfiniteList({ scrollContainer }) {
+    const [loading, setLoading] = useState(false);
+    const [items, setItems] = useState([]);
+
+    function handleLoadMore() {
+        setLoading(true);
+        loadItems(items, items.length).then(newArray => {
+            setLoading(false);
+            setItems(newArray);
+        });
+    }
+
+    const infiniteRef = useInfiniteScroll({
+        loading,
+        // This value is set to "true" for this demo only. You will need to
+        // get this value from the API when you request your items.
+        hasNextPage: true,
+        threshold: 400,
+        onLoadMore: handleLoadMore,
+        scrollContainer
+    });
+
+    return (
+        <List ref={infiniteRef}>
+            {items.map(item => (
+                <ListItem key={item.key}>
+                    {item.value}
+                    <PrintImg />
+                </ListItem>
+            ))}
+            {loading && <ListItem>Loading...</ListItem>}
+        </List>
+    );
+}
+
+InfiniteList.propTypes = {
+    scrollContainer: PropTypes.oneOf(["window", "parent"])
+};
+
+export default InfiniteList;
---
+++ client/src/components/useInfiniteScroll.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+export default function useInfiniteScroll(query, pageNumber) {
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 
+})
+
+    }, [query, pageNumber])
+    return null;
+}
