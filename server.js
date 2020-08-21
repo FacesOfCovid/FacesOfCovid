1620aa1d819420bb731d540f825ef26e9803ea3f
Jed Smit <jedsmit1@gmail.com>
2020-08-21 11:41:56 -0700

    initial server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Middleware    
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+//Routes
+// app.use(routes);
+
+//Database connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/memorialdb", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Database connected, nerd."))
+    .catch(err => console.log(err));
+
+//Start server
+app.listen(PORT, function () {
+    console.log(`==> API Server now listening on PORT ${PORT}!`);
+});
